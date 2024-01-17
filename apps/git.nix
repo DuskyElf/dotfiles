df53a40b607bcb551ed8f86e242a8d4939a24697
DuskyElf <91879372+DuskyElf@users.noreply.github.com>
2024-01-17 20:16:04 +0530

    Indentation deez nuts

--- apps/git.nix
+++ apps/git.nix
@@ -1,7 +1,9 @@
 { config, pkgs, ... } :
 
 {
-    programs.git.enable = true;
-    programs.git.userName = "DuskyElf";
-    programs.git.userEmail = "91879372+DuskyElf@users.noreply.github.com";
+    programs.git = {
+        enable = true;
+        userName = "DuskyElf";
+        userEmail = "91879372+DuskyElf@users.noreply.github.com";
+    };
 }
