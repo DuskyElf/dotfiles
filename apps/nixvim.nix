6fd9899aa2776e3c202aa8b08765261734c0674a
DuskyElf <91879372+DuskyElf@users.noreply.github.com>
2024-01-19 21:16:14 +0530

    keybinds for completions

--- apps/nixvim.nix
+++ apps/nixvim.nix
@@ -82,6 +82,22 @@
           { name = "path"; }
           { name = "buffer"; }
         ];
+
+        mapping = {
+          "<C-Space>" = "cmp.mapping.complete()";
+          "<C-d>" = "cmp.mapping.scroll_docs(-4)";
+          "<C-e>" = "cmp.mapping.close()";
+          "<C-f>" = "cmp.mapping.scroll_docs(4)";
+          "<CR>" = "cmp.mapping.confirm({ select = true })";
+          "<S-Tab>" = {
+            action = "cmp.mapping.select_prev_item()";
+            modes = [ "i" "s" ];
+          };
+          "<Tab>" = {
+            action = "cmp.mapping.select_next_item()";
+            modes = [ "i" "s" ];
+          };
+        };
       };
 
       luasnip.enable = true;
