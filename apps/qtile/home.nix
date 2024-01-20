f99fd346a63362b1c19f8970560d38805d67417a
DuskyElf <91879372+DuskyElf@users.noreply.github.com>
2024-01-20 23:39:18 +0530

    Stylix with Qtile

--- apps/qtile/home.nix
+++ apps/qtile/home.nix
@@ -2,4 +2,23 @@
 
 {
   home.file."/home/duskyelf/.config/qtile/config.py".source = ./config.py;
+
+  home.file."/home/duskyelf/.config/qtile/stylix.py".text = ''
+    base01 = "${config.lib.stylix.colors.base01}"
+    base02 = "${config.lib.stylix.colors.base02}"
+    base03 = "${config.lib.stylix.colors.base03}"
+    base04 = "${config.lib.stylix.colors.base04}"
+    base05 = "${config.lib.stylix.colors.base05}"
+    base06 = "${config.lib.stylix.colors.base06}"
+    base07 = "${config.lib.stylix.colors.base07}"
+    base08 = "${config.lib.stylix.colors.base08}"
+    base09 = "${config.lib.stylix.colors.base09}"
+    base0A = "${config.lib.stylix.colors.base0A}"
+    base0B = "${config.lib.stylix.colors.base0B}"
+    base0C = "${config.lib.stylix.colors.base0C}"
+    base0D = "${config.lib.stylix.colors.base0D}"
+    base0E = "${config.lib.stylix.colors.base0E}"
+    base0F = "${config.lib.stylix.colors.base0F}"
+    font   = "${config.stylix.fonts.monospace.name}"
+'';
 }
