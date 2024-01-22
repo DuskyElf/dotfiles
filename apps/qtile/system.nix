eda01fad0d8c22ee6947775b831367535f009cd6
DuskyElf <91879372+DuskyElf@users.noreply.github.com>
2024-01-22 18:03:27 +0530

    Added qtile-extras

--- apps/qtile/system.nix
+++ apps/qtile/system.nix
@@ -1,6 +1,10 @@
-{ config, pkgs, ... }:
+{ config, pkgs, qtile-extras, ... }:
 
 {
-  environment.systemPackages = with pkgs; [ wlr-randr ];
+  environment.systemPackages = with pkgs; [
+    wlr-randr
+    (python3.withPackages (ps: [ qtile-extras ]))
+  ];
+
   services.xserver.windowManager.qtile.enable = true;
 }
--- flake.nix
+++ flake.nix
@@ -3,6 +3,10 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
+    # nixpkgs-stable.url = "nixpkgs/nixos-23.11";
+
+    # TODO: Update after https://github.com/NixOS/nixpkgs/pull/282869 gets merged
+    working-qtile-extras-package.url = "nixpkgs/9e1ae78a702a764d6815dac293f460f14720d140";
 
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
@@ -14,15 +18,17 @@
     nixvim.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { nixpkgs, stylix, home-manager, nixvim, ... }:
+  outputs = { nixpkgs, stylix, home-manager, nixvim, ... }@inputs:
     let
       lib = nixpkgs.lib;
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
+      qtile-extras = inputs.working-qtile-extras-package.legacyPackages.${system}.python311Packages.qtile-extras;
     in {
       nixosConfigurations = {
         asus-cool = lib.nixosSystem {
           inherit system;
+          extraArgs = { inherit qtile-extras; };
           modules =
             [ ./configuration.nix stylix.nixosModules.stylix ./stylix.nix ];
         };
@@ -31,7 +37,7 @@
       homeConfigurations.duskyelf =
         home-manager.lib.homeManagerConfiguration {
           inherit pkgs;
-          extraSpecialArgs = { nixvim = nixvim; };
+          extraSpecialArgs = { inherit nixvim; };
 
           modules =
             [ ./home.nix stylix.homeManagerModules.stylix ./stylix.nix ];
