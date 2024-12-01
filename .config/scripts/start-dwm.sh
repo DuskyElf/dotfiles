e713b0256237f62d83d021aff83d178bfeff8ddf
DuskyElf <91879372+DuskyElf@users.noreply.github.com>
2024-12-01 22:17:30 +0530

    Fix: Window manager restart

--- .config/scripts/start-dwm.sh
+++ .config/scripts/start-dwm.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# Hardware dependent settings like DPi
-xrdb -merge -I$HOME ~/.Xresources
-xrandr --output "DP-2" --below "eDP-1"
-
 # Utils
 ~/.config/scripts/random-bg.sh &
 
 while true; do
+  # Hardware dependent settings like DPi
+  xrdb -merge -I$HOME ~/.Xresources
+  xrandr --output "DP-2" --below "eDP-1"
+
   dwm 2> ~/.dwm.log
 done
