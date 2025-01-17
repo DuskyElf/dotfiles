4d43117f29695c76f959f81954f6b6caedc74329
DuskyElf <91879372+DuskyElf@users.noreply.github.com>
2025-01-17 17:23:34 +0530

    Add: ExtraStatus in the center with Time

--- suckless/dwmblocks/blocks.def.h
+++ suckless/dwmblocks/blocks.def.h
@@ -3,12 +3,13 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	//{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
 
-	// Center
-	{"", "date '+%I:%M%p'", 10, 0},
-
 	// Right
-	{"|", "~/.config/scripts/status/battery.sh", 5, 0},
+	{"", "~/.config/scripts/status/battery.sh", 5, 0},
 	{"|", "~/.config/scripts/status/volume.sh", 0, 1},
+
+	// Center
+	{";", "date '+%I:%M%p'", 10, 0},
+
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
