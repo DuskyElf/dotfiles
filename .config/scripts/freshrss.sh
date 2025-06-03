6a3a627370312d0fdfe980e4d6fc0dbe5fc3cd12
DuskyElf <91879372+DuskyElf@users.noreply.github.com>
2025-06-03 18:22:11 +0530

    Super fresh FreshRSS

---
+++ .config/scripts/freshrss.sh
@@ -0,0 +1,12 @@
+docker pull freshrss/freshrss
+
+docker rm freshrss
+
+docker run -d --restart unless-stopped --log-opt max-size=10m \
+  -p 127.0.0.1:10001:80 \
+  -e TZ=Asia/Kolkata \
+  -e 'CRON_MIN=1,31' \
+  -v freshrss_data:/var/www/FreshRSS/data \
+  -v freshrss_extensions:/var/www/FreshRSS/extensions \
+  --name freshrss \
+  freshrss/freshrss
